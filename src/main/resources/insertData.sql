73eacad223545b6585a7aebd355236bd6c6235da
Josef Kudláček <j.k.kudlacek@gmail.com>
2024-07-29 09:21:31 +0200

    Fix failing test about insert by SQL

--- src/main/resources/insertData.sql
+++ src/main/resources/insertData.sql
@@ -1,9 +1,8 @@
 INSERT INTO STUDENT(ID, FIRSTNAME, LASTNAME, EMAIL_ADDRESS)
-VALUES (11, 'Student', 'One', 'student.one@luv2code.com')
-    INSERT
-INTO STUDENT(ID, FIRSTNAME, LASTNAME, EMAIL_ADDRESS)
-VALUES (12, 'Student', 'Two', 'student.two@luv2code.com')
+VALUES (11, 'Student', 'One', 'student.one@luv2code.com');
 INSERT INTO STUDENT(ID, FIRSTNAME, LASTNAME, EMAIL_ADDRESS)
-VALUES (13, 'Student', 'Three', 'student.three@luv2code.com')
+VALUES (12, 'Student', 'Two', 'student.two@luv2code.com');
 INSERT INTO STUDENT(ID, FIRSTNAME, LASTNAME, EMAIL_ADDRESS)
-VALUES (14, 'Student', 'Four', 'student.four@luv2code.com')
+VALUES (13, 'Student', 'Three', 'student.three@luv2code.com');
+INSERT INTO STUDENT(ID, FIRSTNAME, LASTNAME, EMAIL_ADDRESS)
+VALUES (14, 'Student', 'Four', 'student.four@luv2code.com');
